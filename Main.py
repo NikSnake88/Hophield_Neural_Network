3741d9c3e7a960045debce51024b7b7bf35082d5
Soma Nervestopheles <nervestopheles@gmail.com>
2022-03-16 11:54:42 +0700

    напитонил

--- Main.py
+++ Main.py
@@ -5,43 +5,64 @@ import os
 
 class iImg():
     def __init__(self, path) -> None:
-        shape = fn.readImage(path)
-        matrx = 
+        self.shape = int(path[-5])
+        self.matrx = fn.readImage(path)
 
 corvX = 0.9
 corvY = 0.1
 
-imagesX = os.listdir(path="./image/80x/") 
-np.random.shuffle(imagesX)
-print(imagesX)
+imageDir = "./image/80x/"
+imagePaths = os.listdir(path=imageDir)
 
 neurons = [a.Neuron(80,80) for i in range(10)]
-
 for i in range(10):
     neurons[i].vread(str(i) +'_V.txt')
 
+imgs = []
+for imgName in imagePaths:
+    imgs.append(iImg(imageDir + imgName))
+print("Imgs download: success")
 
-for i in range(10):
-    for j, imageName in enumerate(imagesX):
-        im = fn.readImage('image/80x/'+ imageName)
-        neuron = neurons[i].sum(im)
-        activ = neurons[i].activation(neuron)
-        #print("Nikita: " + str(neuron))
-        #print("Anton: " + str(activ))
-        if imageName[-5] == str(i):
-            while activ < corvX:
-                fn.corr(activ, corvX, im, neurons[i].vesa)
-                neuron = neurons[i].sum(im)
-                activ = neurons[i].activation(neuron)
-                #print("Nikita: " + str(neuron))
-                #print("Anton: " + str(activ))
-        else:
-            while activ > corvY:
-                fn.corr(activ, corvY, im, neurons[i].vesa)
-                neuron = neurons[i].sum(im)
-                activ = neurons[i].activation(neuron)
-                #print("Nikita: " + str(neuron))
-                #print("Anton: " + str(activ))
+loops = 0
+correcting = 1
+while correcting != 0:
+
+    np.random.shuffle(imgs)
+    correcting = 0
+
+    for img in imgs:
+        for idx, neuron in enumerate(neurons):
+            activ = neuron.activation(neuron.sum(img.matrx))
+            if img.shape == idx and activ < corvX:
+                fn.corr(activ, corvX, img.matrx, neuron.vesa)
+                correcting += 1
+            elif img.shape != idx and activ > corvY:
+                fn.corr(activ, corvY, img.matrx, neuron.vesa)
+                correcting += 1
+    loops += 1
+    print(str(loops) + " " + str(correcting))
+
+# for i in range(10):
+#     for j, imageName in enumerate(imagePaths):
+#         im = fn.readImage(imageDir + imageName)
+#         neuron = neurons[i].sum(im)
+#         activ = neurons[i].activation(neuron)
+#         #print("Nikita: " + str(neuron))
+#         #print("Anton: " + str(activ))
+#         if imageName[-5] == str(i):
+#             while activ < corvX:
+#                 fn.corr(activ, corvX, im, neurons[i].vesa)
+#                 neuron = neurons[i].sum(im)
+#                 activ = neurons[i].activation(neuron)
+#                 #print("Nikita: " + str(neuron))
+#                 #print("Anton: " + str(activ))
+#         else:
+#             while activ > corvY:
+#                 fn.corr(activ, corvY, im, neurons[i].vesa)
+#                 neuron = neurons[i].sum(im)
+#                 activ = neurons[i].activation(neuron)
+#                 #print("Nikita: " + str(neuron))
+#                 #print("Anton: " + str(activ))
 
 for i in range(10):
     neurons[i].vwrite(str(i) +'_V.txt')
